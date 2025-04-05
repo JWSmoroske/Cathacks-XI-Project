497e62209b2a464fe97d6f0a0aebde6c18130e48
Jack Smoroske <jwsmoroske@gmail.com>
2025-04-05 04:51:25 -0400

    updated file names, adding top bar, changed defaults

--- app/src/main/java/com/example/navigationtest/SmallTopAppBar.kt
+++ app/src/main/java/com/example/navigationtest/SmallTopAppBar.kt
@@ -1,21 +1,13 @@
 package com.example.navigationtest
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
